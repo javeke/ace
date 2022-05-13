fc4649b5c5f52e7efeb6f4a00ab24bc80c797f97
Javier <javier.bryan@speurgroup.com>
2022-05-13 01:43:08 -0500

    started the implementation for deleting organizations

---
+++ pages/api/v1/organization/[id].ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { ApplicationResponse } from "../../../../common/types";
+
+// TODO: Complete this api call to the javeke ws to remove organization from DB
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ApplicationResponse>){
+
+}
